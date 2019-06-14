9cb2182ccf48a17cdcb7f6dbf3020c0df38aaba3
Bernardo <bfalese@navent.com>
2019-06-14 16:10:11 -0300

    solo metric de escalado por prometheus

--- src/main/java/com/navent/realestate/metrics/MetricsConfig.java
+++ src/main/java/com/navent/realestate/metrics/MetricsConfig.java
@@ -20,11 +20,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.net.InetAddress;
+import java.util.function.Predicate;
 
+import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.config.NamingConvention;
 import io.micrometer.jmx.JmxConfig;
 import io.micrometer.jmx.JmxMeterRegistry;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
 import io.micrometer.spring.autoconfigure.MetricsProperties;
 import io.micrometer.spring.web.servlet.WebMvcTagsProvider;
@@ -54,38 +58,48 @@ public class MetricsConfig extends WebMvcConfigurerAdapter {
 	public WebMvcTagsProvider servletTagsProvider() {
 		return new CustomWebMvcTagsProvider();
 	}
-	
+
 	@Bean
-    public MeterRegistryCustomizer<JmxMeterRegistry> graphiteMetricsNamingConvention() {
-      return registry -> registry.config().namingConvention(NamingConvention.camelCase);
-    }
+	public MeterRegistryCustomizer<JmxMeterRegistry> jmxMetricsNamingConvention() {
+		return registry -> registry.config().namingConvention(NamingConvention.camelCase);
+	}
+
+	@Bean
+	public MeterRegistryCustomizer<PrometheusMeterRegistry> prometheusMetricsNamingConvention(
+			MetricsProperties properties) {
+		return registry -> registry.config().meterFilter(MeterFilter.deny(new Predicate<Meter.Id>() {
+			@Override
+			public boolean test(Meter.Id id) {
+				return !id.getName().equals(
+						properties.getWeb().getServer().getRequestsMetricName() + ".uri.root.1.min.request.rate");
+			}
+		}));
+	}
 
 	@SuppressWarnings("deprecation")
-    @Bean
-    public NaventWebMvcMetricsFilter naventWebMetricsFilter(MeterRegistry registry,
-                                                WebMvcTagsProvider tagsProvider,
-                                                WebApplicationContext ctx,
-                                                MetricsProperties properties,
-                                                NaventMetricsProperties naventProperties) {
-        return new NaventWebMvcMetricsFilter(registry, tagsProvider,
-                properties.getWeb().getServer().getRequestsMetricName(),
-                true,
-                new HandlerMappingIntrospector(ctx),
-                naventProperties);
-    }
+	@Bean
+	public NaventWebMvcMetricsFilter naventWebMetricsFilter(MeterRegistry registry, WebMvcTagsProvider tagsProvider,
+			WebApplicationContext ctx, MetricsProperties properties, NaventMetricsProperties naventProperties) {
+		return new NaventWebMvcMetricsFilter(registry, tagsProvider,
+				properties.getWeb().getServer().getRequestsMetricName(), true, new HandlerMappingIntrospector(ctx),
+				naventProperties);
+	}
 
 	@Bean
 	@Autowired
-	public EndpointMetricsProvider endpointMetricsProvider(JmxConfig config, NaventMetricsProperties metricsProperties) {
-		if(metricsProperties.getApdex().isEnabled()) {
-			Assert.notNull(metricsProperties.getApdex().getMillis(), "Metrics apdex satisfied is mandatory with apdex enabled");
+	public EndpointMetricsProvider endpointMetricsProvider(JmxConfig config,
+			NaventMetricsProperties metricsProperties) {
+		if (metricsProperties.getApdex().isEnabled()) {
+			Assert.notNull(metricsProperties.getApdex().getMillis(),
+					"Metrics apdex satisfied is mandatory with apdex enabled");
 		}
 		return new EndpointMetricsProvider(config, metricsProperties);
 	}
 
 	@Bean
 	@Autowired
-	public ZabbixAgent zabbixAgent(EndpointMetricsProvider endpointMetricsProvider, NaventMetricsProperties metricsProperties) throws Exception {
+	public ZabbixAgent zabbixAgent(EndpointMetricsProvider endpointMetricsProvider,
+			NaventMetricsProperties metricsProperties) throws Exception {
 		ZabbixAgent agent = new ZabbixAgent();
 		agent.setEnableActive(true);
 		agent.setEnablePassive(false);
