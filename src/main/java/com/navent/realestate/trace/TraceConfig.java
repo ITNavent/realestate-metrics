3ec832f4be4cf3af225ebc7bda996e23f5ecdf68
Bernardo Falese <bfalese@navent.com>
2020-03-06 12:30:21 -0300

    tracing

--- src/main/java/com/navent/realestate/trace/TraceConfig.java
+++ src/main/java/com/navent/realestate/trace/TraceConfig.java
@@ -4,33 +4,23 @@ import com.navent.realestate.metrics.NaventMetricsProperties;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.Assert;
-
-import brave.Tracing;
-import brave.opentracing.BraveTracer;
-import io.opentracing.Tracer;
-import zipkin2.Span;
-import zipkin2.reporter.AsyncReporter;
-import zipkin2.reporter.Sender;
-import zipkin2.reporter.urlconnection.URLConnectionSender;
 
 @Configuration
 @EnableConfigurationProperties(NaventMetricsProperties.class)
 @ConditionalOnProperty(name = "metrics.trace.enabled", havingValue = "true", matchIfMissing = false)
 public class TraceConfig {
 
-	@Bean
-	public io.opentracing.Tracer tracer(NaventMetricsProperties metricsProperties) {
-		Assert.notNull(metricsProperties.getTrace().getSenderEndpoint(), "Metrics Trace sender endpoint is mandatory");
-		Assert.notNull(metricsProperties.getTrace().getServiceName(), "Metrics Trace service name is mandatory");
-
-		Sender sender = URLConnectionSender.create(metricsProperties.getTrace().getSenderEndpoint());
-	    AsyncReporter<Span> reporter = AsyncReporter.builder(sender).build();
-	    Tracing braveTracer = Tracing.newBuilder().localServiceName(metricsProperties.getTrace().getServiceName())
-	    		.spanReporter(reporter).build();
-	    Tracer tracer = BraveTracer.create(braveTracer);
-	    return tracer;
-	}
+//	@Bean
+//	public io.opentracing.Tracer tracer(NaventMetricsProperties metricsProperties) {
+//		Assert.notNull(metricsProperties.getTrace().getSenderEndpoint(), "Metrics Trace sender endpoint is mandatory");
+//		Assert.notNull(metricsProperties.getTrace().getServiceName(), "Metrics Trace service name is mandatory");
+//
+//		Sender sender = URLConnectionSender.create(metricsProperties.getTrace().getSenderEndpoint());
+//	    AsyncReporter<Span> reporter = AsyncReporter.builder(sender).build();
+//	    Tracing braveTracer = Tracing.newBuilder().localServiceName(metricsProperties.getTrace().getServiceName())
+//	    		.spanReporter(reporter).build();
+//	    Tracer tracer = BraveTracer.create(braveTracer);
+//	    return tracer;
+//	}
 }
